ea4046339a6561d3577498da0cddccbe2b4e9331
etienne86 <barbier.et@gmail.com>
2020-04-20 10:32:01 +0200

    adapt settings.py with database password for production

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -96,17 +96,29 @@ SITE_ID = 1
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pur_beurre_db',
-        'USER': 'ebarbier',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '5432',
+if os.environ.get('ENV') == 'PRODUCTION':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'pur_beurre_db',
+            'USER': 'ebarbier',
+            'PASSWORD': 'etienneb',
+            'HOST': '',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'pur_beurre_db',
+            'USER': 'ebarbier',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '5432',
+        }
+    }
+
 
 # Authentication
 # User substitution
