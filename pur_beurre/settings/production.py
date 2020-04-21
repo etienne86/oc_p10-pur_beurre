982261a8dfa76e8c37b77aa03b701ef16ab2d0db
etienne86 <barbier.et@gmail.com>
2020-04-21 15:04:17 +0200

    add sentry configuration

--- pur_beurre/settings/production.py
+++ pur_beurre/settings/production.py
@@ -2,6 +2,20 @@ from . import *
 
 import os
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+
+sentry_sdk.init(
+    dsn="https://c42d683c1779454f9b50c0e420af723a@o380264.ingest.sentry.io/5205889",
+    integrations=[DjangoIntegration()],
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 ALLOWED_HOSTS = ['178.62.10.30']
