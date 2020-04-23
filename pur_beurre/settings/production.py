5a4975906c7e69b5b8d6ce2ca348c13fcec1e348
etienne86 <barbier.et@gmail.com>
2020-04-23 08:18:28 +0200

    add level INFO to logging

--- pur_beurre/settings/production.py
+++ pur_beurre/settings/production.py
@@ -1,14 +1,21 @@
 from . import *
 
 import os
+import logging
 
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.logging import LoggingIntegration
 
+
+sentry_logging = LoggingIntegration(
+    level=logging.INFO,        # Capture info and above as breadcrumbs
+    event_level=logging.INFO   # Send errors as events
+)
 
 sentry_sdk.init(
     dsn="https://c42d683c1779454f9b50c0e420af723a@o380264.ingest.sentry.io/5205889",
-    integrations=[DjangoIntegration()],
+    integrations=[DjangoIntegration(), sentry_logging],
 
     # If you wish to associate users to errors (assuming you are using
     # django.contrib.auth) you may enable sending PII data.
